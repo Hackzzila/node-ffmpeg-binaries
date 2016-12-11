76659cb83432233e24b6dcc738fc90b27cd02c6e
Zachary Vacura <zvacura@outlook.com>
2016-12-11 12:26:03 -0600

    Updates

--- install.js
+++ install.js
@@ -1,15 +1,24 @@
 'use strict';
 
+const fs = require('fs');
 const targz = require('tar.gz');
+const superagent = require('superagent');
 
+let request;
 if (process.platform === 'win32') {
-  targz().extract('./windows.tar.gz', 'bin')
-    .catch((err) => {
-      throw err;
-    });
+  request = superagent.get('https://github.com/Hackzzila/node-ffmpeg-binaries/blob/master/windows.tar.gz?raw=true');
 } else if (process.platform === 'linux') {
-  targz().extract('./linux.tar.gz', 'bin')
+  request = superagent.get('https://github.com/Hackzzila/node-ffmpeg-binaries/blob/master/linux.tar.gz?raw=true');
+}
+
+console.log('Downloading')
+request.pipe(fs.createWriteStream('./bin.tar.gz'));
+
+request.on('end', () => {
+  console.log('Extracting');
+  targz().extract('./bin.tar.gz', 'bin')
+    .then(() => console.log('Done!'))
     .catch((err) => {
       throw err;
     });
-}
+});
