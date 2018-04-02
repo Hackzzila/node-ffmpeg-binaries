78a826b266fe15261720d678ad5bffce97ce61a5
Shayne Har <ShayneEHartford@gmail.Com>
2018-04-02 14:17:44 -0400

    Merged .eslintrc.js into package.json Updated package-lock.json Destructured some things Updated README with table

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const path = require('path');
+const { join } = require('path');
 
-module.exports = path.join(__dirname, 'bin', `ffmpeg${process.platform === 'win32' ? '.exe' : ''}`);
+module.exports = join(__dirname, 'bin', `ffmpeg${process.platform === 'win32' ? '.exe' : ''}`);
