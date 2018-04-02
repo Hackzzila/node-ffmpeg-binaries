acd235413549103c58fbe9986b97ad1ae743ee1b
Hackzzila <admin@hackzzila.com>
2018-04-02 14:10:31 -0500

    improvements

--- install.js
+++ install.js
@@ -4,51 +4,49 @@ const decompress = require('decompress');
 const tarxz = require('decompress-tarxz');
 const unzip = require('decompress-unzip');
 
-function callback(plugin) {
-  return (res) => {
-    let last;
-    let complete = 0;
-    const total = parseInt(res.headers['content-length'], 10);
+function callback(res) {
+  let last;
+  let complete = 0;
+  const total = parseInt(res.headers['content-length'], 10);
 
-    let index = 0;
-    const buf = Buffer.alloc(total);
+  let index = 0;
+  const buf = Buffer.alloc(total);
 
-    res.on('data', (chunk) => {
-      chunk.copy(buf, index);
-      index += chunk.length;
+  res.on('data', (chunk) => {
+    chunk.copy(buf, index);
+    index += chunk.length;
 
-      complete += chunk.length;
-      const progress = Math.round((complete / total) * 20);
+    complete += chunk.length;
+    const progress = Math.round((complete / total) * 20);
 
-      if (progress !== last) {
-        readline.cursorTo(process.stdout, 0, null);
-
-        process.stdout.write(`Downloading binary: [${'='.repeat(progress)}${[' '.repeat(20 - progress)]}] ${Math.round((complete / total) * 100)}%`);
-
-        last = progress;
-      }
-    });
-
-    res.on('end', () => {
+    if (progress !== last) {
       readline.cursorTo(process.stdout, 0, null);
-      console.log(`Downloading binary: [${'='.repeat(20)}] 100%`);
 
-      decompress(buf, 'bin', {
-        plugins: [plugin()],
-        strip: process.platform === 'linux' ? 1 : 2,
-        filter: x => x.path === (process.platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg'),
-      });
+      process.stdout.write(`Downloading binary: [${'='.repeat(progress)}${[' '.repeat(20 - progress)]}] ${Math.round((complete / total) * 100)}%`);
+
+      last = progress;
+    }
+  });
+
+  res.on('end', () => {
+    readline.cursorTo(process.stdout, 0, null);
+    console.log(`Downloading binary: [${'='.repeat(20)}] 100%`);
+
+    decompress(buf, 'bin', {
+      plugins: process.platform === 'linux' ? [tarxz()] : [unzip()],
+      strip: process.platform === 'linux' ? 1 : 2,
+      filter: x => x.path === (process.platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg'),
     });
-  };
+  });
 }
 
 if (process.platform === 'win32') {
   switch (process.arch) {
     case 'x64':
-      http.get('https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip', callback(unzip));
+      http.get('https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip', callback);
       break;
     case 'ia32':
-      http.get('https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip', callback(unzip));
+      http.get('https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip', callback);
       break;
     default:
       throw new Error('unsupported platform');
@@ -56,16 +54,16 @@ if (process.platform === 'win32') {
 } else if (process.platform === 'linux') {
   switch (process.arch) {
     case 'x64':
-      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz', callback(tarxz));
+      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz', callback);
       break;
     case 'ia32':
-      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz', callback(tarxz));
+      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz', callback);
       break;
     case 'arm':
-      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armhf-32bit-static.tar.xz', callback(tarxz));
+      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armhf-32bit-static.tar.xz', callback);
       break;
     case 'arm64':
-      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-64bit-static.tar.xz', callback(tarxz));
+      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-64bit-static.tar.xz', callback);
       break;
     default:
       throw new Error('unsupported platform');
@@ -73,7 +71,7 @@ if (process.platform === 'win32') {
 } else if (process.platform === 'darwin') {
   switch (process.arch) {
     case 'x64':
-      http.get('https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-latest-macos64-static.zip', callback(unzip));
+      http.get('https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-latest-macos64-static.zip', callback);
       break;
     default:
       throw new Error('unsupported platform');
