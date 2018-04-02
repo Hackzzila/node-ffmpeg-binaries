009e4d540aa5bda01cc6781b9f1d1c682260c2ef
Hackzzila <admin@hackzzila.com>
2018-04-02 11:38:29 -0500

    rewrite

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
 # ffmpeg-binaries
-A simple way to install FFMPEG globally or for an individual project.  
-Supports Windows, Linux and macOS
+A simple way to install FFmpeg globally or for an individual project.  
+Supports Windows (ia32, x64), Linux (ia32, x64, arm, arm64) and macOS (x64)
 
-## Versioning
-The major, minor, and patch versions correspond to the version of ffmpeg this package supports.
-The pre-release version corresponds to the version of the package itself.
+## API
+`ffmpeg-binaries` exports a string containing the path to the FFmpeg executable.
 
-## Install
-### Install Globally:
-`npm i -g ffmpeg-binaries`  
-This will put `ffmpeg` and `ffprobe` in the path.
 
-### Install For a Project:
-`npm i ffmpeg-binaries`  
-If you are on Windows his will put `ffmpeg`, `ffprobe`, and `ffplay` in `node_modules/ffmpeg-binaries/bin`  
-If you are on Linux or macOS his will put `ffmpeg`, `ffprobe`, and `ffserver` in `node_modules/ffmpeg-binaries/bin`
+This software uses code of <a href=http://ffmpeg.org>FFmpeg</a> licensed under the <a href=http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html>LGPLv2.1</a> and its source can be downloaded [here](ffmpeg)
--- install.js
+++ install.js
@@ -1,22 +1,81 @@
-'use strict';
-
-const zlib = require('zlib');
-const tar = require('tar');
-const path = require('path');
 const http = require('https');
+const readline = require('readline');
+const decompress = require('decompress');
+const tarxz = require('decompress-tarxz');
+const unzip = require('decompress-unzip');
 
-function callback(res) {
-  res
-    .pipe(zlib.createGunzip())
-    .pipe(tar.Extract({ path: path.join(__dirname, 'bin') }));
+function callback(plugin) {
+  return (res) => {
+    let buf = Buffer.alloc(0);
+
+    let last;
+    let complete = 0;
+    const total = res.headers['content-length'];
+
+    res.on('data', (chunk) => {
+      buf = Buffer.concat([buf, chunk]);
+
+      complete += chunk.length;
+      const progress = Math.round((complete / total) * 20);
+
+      if (progress !== last) {
+        readline.cursorTo(process.stdout, 0, null);
+
+        process.stdout.write(`Downloading binary: [${'='.repeat(progress)}${[' '.repeat(20 - progress)]}] ${Math.round((complete / total) * 100)}%`);
+
+        last = progress;
+      }
+    });
+
+    res.on('end', () => {
+      readline.cursorTo(process.stdout, 0, null);
+      process.stdout.write(`Downloading binary: [${'='.repeat(20)}] 100%`);
+
+      decompress(buf, 'bin', {
+        plugins: [plugin()],
+        strip: process.platform === 'linux' ? 1 : 2,
+        filter: x => x.path === (process.platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg'),
+      });
+    });
+  };
 }
 
 if (process.platform === 'win32') {
-  http.get('https://raw.githubusercontent.com/Hackzzila/node-ffmpeg-binaries/master/windows.tar.gz', callback);
+  switch (process.arch) {
+    case 'x64':
+      http.get('https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip', callback(unzip));
+      break;
+    case 'ia32':
+      http.get('https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip', callback(unzip));
+      break;
+    default:
+      throw new Error('unsupported platform');
+  }
 } else if (process.platform === 'linux') {
-  http.get('https://raw.githubusercontent.com/Hackzzila/node-ffmpeg-binaries/master/linux.tar.gz', callback);
+  switch (process.arch) {
+    case 'x64':
+      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz', callback(tarxz));
+      break;
+    case 'ia32':
+      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz', callback(tarxz));
+      break;
+    case 'arm':
+      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armhf-32bit-static.tar.xz', callback(tarxz));
+      break;
+    case 'arm64':
+      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-64bit-static.tar.xz', callback(tarxz));
+      break;
+    default:
+      throw new Error('unsupported platform');
+  }
 } else if (process.platform === 'darwin') {
-  http.get('https://raw.githubusercontent.com/Hackzzila/node-ffmpeg-binaries/master/darwin.tar.gz', callback);
+  switch (process.arch) {
+    case 'x64':
+      http.get('https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-latest-macos64-static.zip', callback(unzip));
+      break;
+    default:
+      throw new Error('unsupported platform');
+  }
 } else {
   throw new Error('unsupported platform');
 }
