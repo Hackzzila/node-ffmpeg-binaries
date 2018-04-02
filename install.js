f910b31dc5be292ce18cc5eab11865a445afb475
Shayne Hartford <ShayneEHartford@Gmail.Com>
2018-04-02 15:23:28 -0400

    Fix get

--- install.js
+++ install.js
@@ -43,10 +43,10 @@ function callback(res) {
 if (process.platform === 'win32') {
   switch (process.arch) {
     case 'x64':
-      http.get('https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip', callback);
+      get('https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-latest-win64-static.zip', callback);
       break;
     case 'ia32':
-      http.get('https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip', callback);
+      get('https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-latest-win32-static.zip', callback);
       break;
     default:
       throw new Error('unsupported platform');
@@ -54,16 +54,16 @@ if (process.platform === 'win32') {
 } else if (process.platform === 'linux') {
   switch (process.arch) {
     case 'x64':
-      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz', callback);
+      get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz', callback);
       break;
     case 'ia32':
-      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz', callback);
+      get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-32bit-static.tar.xz', callback);
       break;
     case 'arm':
-      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armhf-32bit-static.tar.xz', callback);
+      get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-armhf-32bit-static.tar.xz', callback);
       break;
     case 'arm64':
-      http.get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-64bit-static.tar.xz', callback);
+      get('https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-arm64-64bit-static.tar.xz', callback);
       break;
     default:
       throw new Error('unsupported platform');
@@ -71,7 +71,7 @@ if (process.platform === 'win32') {
 } else if (process.platform === 'darwin') {
   switch (process.arch) {
     case 'x64':
-      http.get('https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-latest-macos64-static.zip', callback);
+      get('https://ffmpeg.zeranoe.com/builds/macos64/static/ffmpeg-latest-macos64-static.zip', callback);
       break;
     default:
       throw new Error('unsupported platform');
